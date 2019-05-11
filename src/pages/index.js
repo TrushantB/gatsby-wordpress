b5c4a0002440c43930b93ea7fc1a194fe8ed6209
Trushant <trushant.ivision@gmail.com>
2019-05-11 19:51:37 +0530

    fixed redirect to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Page from '../templates/page.js'
+const IndexPage = () =>{
+  if (typeof window !== '/') {
+    window.location = '/home';
+  }
 
-const IndexPage = () => (
+  return null;
+  return (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
@@ -17,5 +23,7 @@ const IndexPage = () => (
     <Link to="/home/">Go to Home page</Link>
   </Layout>
 )
+}
+
 
 export default IndexPage
